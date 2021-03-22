06605298af1f50f0125c15af06abfc1053301737
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2021-03-21 23:13:04 -0600

    Try the navbar from bit

--- asclepio/src/App.tsx
+++ asclepio/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Navbar from "commons/Navbar/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar/>
+      </Router>
     </div>
   );
 }
